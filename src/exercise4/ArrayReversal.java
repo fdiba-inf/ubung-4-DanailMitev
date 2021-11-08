30c00e552a742d46c2ca3b0ba4b49d32ce5131df
DanailMitev <>
2021-11-08 20:41:37 +0000

    Ubung4

--- src/exercise4/ArrayReversal.java
+++ src/exercise4/ArrayReversal.java
@@ -1,4 +1,5 @@
 package exercise4;
+
 import java.util.Scanner;
 import java.util.Arrays;
 import java.lang.Math;
@@ -7,20 +8,25 @@ public class ArrayReversal {
 
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int size = input.nextInt();
-    char ch[] = new char[size];
-    for (int i = 0; i < size; i++) {
-      ch[i] = input.next().charAt(0);
-    }
-    System.out.print("[");
-    for (int i = size- 1; i >= 0; i--) {
-      if (1 > 0) {
-        System.out.print(ch[i] + ", ");
-      } else {
-        System.out.print(ch[i]);
-      }
-    }
-    System.out.print("]");
 
+    System.out.print("Enter size: ");
+    int size = input.nextInt();
+
+    char[] symbols = new char[size];
+
+    System.out.println("Enter numbers: ");
+    for (int index = 0; index < symbols.length; index++) {
+      symbols[index] = input.next().charAt(0);
+    }
+
+    char[] reversedSymbols = new char[size];
+    int reversedIndex = 0;
+    for (int index = symbols.length - 1; index >= 0; index--) {
+      reversedSymbols[reversedIndex] = symbols[index];
+      reversedIndex++;
+  }
+
+  String reversedString = Arrays.toString(reversedSymbols);
+  System.out.println("Reversed symbols: " + reversedString);
   }
 }
